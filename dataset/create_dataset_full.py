0410004039b86d1f405c4d2bdee92d09a577e857
Michael Liang <michaelliang15@gmail.com>
2024-11-05 03:40:53 +0000

    create ds

--- dataset/create_dataset_full.py
+++ dataset/create_dataset_full.py
@@ -12,6 +12,8 @@ from sklearn.model_selection import train_test_split
 import datasets
 import math
 from datasets import Dataset
+import pyarrow as pa
+
 def print_rows_removed(before_count, after_df, step_description, column_to_check=None):
     after_count = len(after_df)
     rows_removed = before_count - after_count
@@ -74,71 +76,94 @@ if 'AL_index' not in filtered_df.columns:
 if 'SYM_H' not in filtered_df.columns:
     filtered_df['SYM_H'] = np.nan
 
-# -----------------------------------
-# Add magnetic field, plasma, solar indices, particle data
-# -----------------------------------
+# # -----------------------------------
+# # Add magnetic field, plasma, solar indices, particle data
+# # -----------------------------------
 
-# Parse into datetime object
-filtered_df['DateTimeFormatted'] = pd.to_datetime(filtered_df['DateTimeFormatted'], format='%Y-%m-%d %H:%M:%S')
+# # Parse into datetime object
+# filtered_df['DateTimeFormatted'] = pd.to_datetime(filtered_df['DateTimeFormatted'], format='%Y-%m-%d %H:%M:%S')
 
-# Read the single OMNI data file
-omni_file_path = '../data/omni_magnetic_plasma_solar/omni2_K1CK9l7koS.lst'
+# # Read the single OMNI data file
+# omni_file_path = '../data/omni_magnetic_plasma_solar/omni2_K1CK9l7koS.lst'
 
-# Define column names
-columns = [
-    'Year', 'Day', 'Hour', 
-    'Mag_Scalar_B', 'Mag_Vector_B', 'Mag_B_Lat_GSE', 'Mag_B_Long_GSE',
-    'Mag_BX_GSE', 'Mag_BY_GSE', 'Mag_BZ_GSE', 'Mag_BY_GSM', 'Mag_BZ_GSM', 
-    'Mag_RMS_Mag', 'Mag_RMS_Vector', 'Mag_RMS_BX_GSE', 'Mag_RMS_BY_GSE', 'Mag_RMS_BZ_GSE',
-    'Plasma_SW_Temp', 'Plasma_SW_Density', 'Plasma_SW_Speed',
-    'Plasma_SW_Flow_Long', 'Plasma_SW_Flow_Lat', 'Plasma_Alpha_Prot_Ratio', 
-    'Plasma_Sigma_T', 'Plasma_Sigma_N', 'Plasma_Sigma_V', 
-    'Plasma_Sigma_Phi_V', 'Plasma_Sigma_Theta_V', 'Plasma_Sigma_Ratio',
-    'Solar_Kp', 'Solar_R_Sunspot', 'Solar_Dst', 'Solar_Ap', 
-    'Solar_AE', 'Solar_AL', 'Solar_AU', 'Solar_PC',
-    'Solar_Lyman_Alpha',
-    'Particle_Proton_Flux_1MeV', 'Particle_Proton_Flux_2MeV', 
-    'Particle_Proton_Flux_4MeV', 'Particle_Proton_Flux_10MeV', 
-    'Particle_Proton_Flux_30MeV', 'Particle_Proton_Flux_60MeV',
-    'Particle_Flux_Flag'
-]
+# # Define column names
+# columns = [
+#     'Year', 'Day', 'Hour', 
+#     'Mag_Scalar_B', 'Mag_Vector_B', 'Mag_B_Lat_GSE', 'Mag_B_Long_GSE',
+#     'Mag_BX_GSE', 'Mag_BY_GSE', 'Mag_BZ_GSE', 'Mag_BY_GSM', 'Mag_BZ_GSM', 
+#     'Mag_RMS_Mag', 'Mag_RMS_Vector', 'Mag_RMS_BX_GSE', 'Mag_RMS_BY_GSE', 'Mag_RMS_BZ_GSE',
+#     'Plasma_SW_Temp', 'Plasma_SW_Density', 'Plasma_SW_Speed',
+#     'Plasma_SW_Flow_Long', 'Plasma_SW_Flow_Lat', 'Plasma_Alpha_Prot_Ratio', 
+#     'Plasma_Sigma_T', 'Plasma_Sigma_N', 'Plasma_Sigma_V', 
+#     'Plasma_Sigma_Phi_V', 'Plasma_Sigma_Theta_V', 'Plasma_Sigma_Ratio',
+#     'Solar_Kp', 'Solar_R_Sunspot', 'Solar_Dst', 'Solar_Ap', 
+#     'Solar_AE', 'Solar_AL', 'Solar_AU', 'Solar_PC',
+#     'Solar_Lyman_Alpha',
+#     'Particle_Proton_Flux_1MeV', 'Particle_Proton_Flux_2MeV', 
+#     'Particle_Proton_Flux_4MeV', 'Particle_Proton_Flux_10MeV', 
+#     'Particle_Proton_Flux_30MeV', 'Particle_Proton_Flux_60MeV',
+#     'Particle_Flux_Flag'
+# ]
 
-# Read the OMNI data file
-omni_df = pd.read_csv(omni_file_path, delim_whitespace=True, names=columns)
+# # Read the OMNI data file
+# omni_df = pd.read_csv(omni_file_path, delim_whitespace=True, names=columns)
 
-# Create the 'DateTime' column from Year, Day of Year, Hour, and Minute
-omni_df['DateTime'] = pd.to_datetime(omni_df['Year'] * 1000 + omni_df['Day'], format='%Y%j') \
-                 + pd.to_timedelta(omni_df['Hour'], unit='h')
+# # Create the 'DateTime' column from Year, Day of Year, Hour, and Minute
+# omni_df['DateTime'] = pd.to_datetime(omni_df['Year'] * 1000 + omni_df['Day'], format='%Y%j') \
+#                  + pd.to_timedelta(omni_df['Hour'], unit='h')
 
-# Keep only the necessary columns
-omni_df = omni_df[['DateTime', 'Mag_Scalar_B', 'Mag_Vector_B', 'Mag_B_Lat_GSE', 'Mag_B_Long_GSE',
-         'Mag_BX_GSE', 'Mag_BY_GSE', 'Mag_BZ_GSE', 'Mag_BY_GSM', 'Mag_BZ_GSM', 
-         'Mag_RMS_Mag', 'Mag_RMS_Vector', 'Mag_RMS_BX_GSE', 'Mag_RMS_BY_GSE', 'Mag_RMS_BZ_GSE',
-         'Plasma_SW_Temp', 'Plasma_SW_Density', 'Plasma_SW_Speed',
-         'Plasma_SW_Flow_Long', 'Plasma_SW_Flow_Lat', 'Plasma_Alpha_Prot_Ratio', 
-         'Plasma_Sigma_T', 'Plasma_Sigma_N', 'Plasma_Sigma_V', 
-         'Plasma_Sigma_Phi_V', 'Plasma_Sigma_Theta_V', 'Plasma_Sigma_Ratio',
-         'Solar_Kp', 'Solar_R_Sunspot', 'Solar_Dst', 'Solar_Ap', 
-         'Solar_AE', 'Solar_AL', 'Solar_AU', 'Solar_PC',
-         'Solar_Lyman_Alpha',
-         'Particle_Proton_Flux_1MeV', 'Particle_Proton_Flux_2MeV', 
-         'Particle_Proton_Flux_4MeV', 'Particle_Proton_Flux_10MeV', 
-         'Particle_Proton_Flux_30MeV', 'Particle_Proton_Flux_60MeV',
-         'Particle_Flux_Flag']]
+# # Keep only the necessary columns
+# omni_df = omni_df[['DateTime', 'Mag_Scalar_B', 'Mag_Vector_B', 'Mag_B_Lat_GSE', 'Mag_B_Long_GSE',
+#          'Mag_BX_GSE', 'Mag_BY_GSE', 'Mag_BZ_GSE', 'Mag_BY_GSM', 'Mag_BZ_GSM', 
+#          'Mag_RMS_Mag', 'Mag_RMS_Vector', 'Mag_RMS_BX_GSE', 'Mag_RMS_BY_GSE', 'Mag_RMS_BZ_GSE',
+#          'Plasma_SW_Temp', 'Plasma_SW_Density', 'Plasma_SW_Speed',
+#          'Plasma_SW_Flow_Long', 'Plasma_SW_Flow_Lat', 'Plasma_Alpha_Prot_Ratio', 
+#          'Plasma_Sigma_T', 'Plasma_Sigma_N', 'Plasma_Sigma_V', 
+#          'Plasma_Sigma_Phi_V', 'Plasma_Sigma_Theta_V', 'Plasma_Sigma_Ratio',
+#          'Solar_Kp', 'Solar_R_Sunspot', 'Solar_Dst', 'Solar_Ap', 
+#          'Solar_AE', 'Solar_AL', 'Solar_AU', 'Solar_PC',
+#          'Solar_Lyman_Alpha',
+#          'Particle_Proton_Flux_1MeV', 'Particle_Proton_Flux_2MeV', 
+#          'Particle_Proton_Flux_4MeV', 'Particle_Proton_Flux_10MeV', 
+#          'Particle_Proton_Flux_30MeV', 'Particle_Proton_Flux_60MeV',
+#          'Particle_Flux_Flag']]
 
-# Reset index to make 'DateTime' a column
-omni_df.reset_index(drop=True, inplace=True)
-omni_df = omni_df.drop_duplicates(subset='DateTime', keep='first')
-omni_df.set_index('DateTime', inplace=True)
-filtered_df['DateTimeFormatted_copy'] = filtered_df['DateTimeFormatted']
-filtered_df['DateTimeFormatted_copy'] = filtered_df['DateTimeFormatted_copy'].dt.round('H')
-filtered_df.set_index('DateTimeFormatted_copy', inplace=True)
-filtered_df.sort_index(inplace=True)
+# # Reset index to make 'DateTime' a column
+# omni_df.reset_index(drop=True, inplace=True)
+# omni_df = omni_df.drop_duplicates(subset='DateTime', keep='first')
+# omni_df.set_index('DateTime', inplace=True)
+# filtered_df['DateTimeFormatted_copy'] = filtered_df['DateTimeFormatted']
+# filtered_df['DateTimeFormatted_copy'] = filtered_df['DateTimeFormatted_copy'].dt.round('H')
+# filtered_df.set_index('DateTimeFormatted_copy', inplace=True)
+# filtered_df.sort_index(inplace=True)
 
-omni_df.sort_index(inplace=True)
+# omni_df.sort_index(inplace=True)
 
-# Instantaneous
-# for col in ['Mag_Scalar_B', 'Mag_Vector_B', 'Mag_B_Lat_GSE', 'Mag_B_Long_GSE',
+# # Instantaneous
+# # for col in ['Mag_Scalar_B', 'Mag_Vector_B', 'Mag_B_Lat_GSE', 'Mag_B_Long_GSE',
+# #             'Mag_BX_GSE', 'Mag_BY_GSE', 'Mag_BZ_GSE', 'Mag_BY_GSM', 'Mag_BZ_GSM', 
+# #             'Mag_RMS_Mag', 'Mag_RMS_Vector', 'Mag_RMS_BX_GSE', 'Mag_RMS_BY_GSE', 'Mag_RMS_BZ_GSE',
+# #             'Plasma_SW_Temp', 'Plasma_SW_Density', 'Plasma_SW_Speed',
+# #             'Plasma_SW_Flow_Long', 'Plasma_SW_Flow_Lat', 'Plasma_Alpha_Prot_Ratio', 
+# #             'Plasma_Sigma_T', 'Plasma_Sigma_N', 'Plasma_Sigma_V', 
+# #             'Plasma_Sigma_Phi_V', 'Plasma_Sigma_Theta_V', 'Plasma_Sigma_Ratio',
+# #             'Solar_Kp', 'Solar_R_Sunspot', 'Solar_Dst', 'Solar_Ap', 
+# #             'Solar_AE', 'Solar_AL', 'Solar_AU', 'Solar_PC',
+# #             'Solar_Lyman_Alpha',
+# #             'Particle_Proton_Flux_1MeV', 'Particle_Proton_Flux_2MeV', 
+# #             'Particle_Proton_Flux_4MeV', 'Particle_Proton_Flux_10MeV', 
+# #             'Particle_Proton_Flux_30MeV', 'Particle_Proton_Flux_60MeV', 'Particle_Flux_Flag']:
+# #     if col not in filtered_df.columns:
+# #         filtered_df[col] = pd.Series(np.nan, index=filtered_df.index)
+# #     filtered_df[col] = filtered_df[col].fillna(omni_df[col])
+
+# # Temporal
+# low_res_time_range = pd.timedelta_range(start='0D', end='3D', freq='1H')
+# dt_index = pd.DatetimeIndex(filtered_df.index)
+# low_res_timestamps = dt_index.values[:, None] - low_res_time_range.values
+
+# new_columns = {}
+# for col in tqdm(['Mag_Scalar_B', 'Mag_Vector_B', 'Mag_B_Lat_GSE', 'Mag_B_Long_GSE',
 #             'Mag_BX_GSE', 'Mag_BY_GSE', 'Mag_BZ_GSE', 'Mag_BY_GSM', 'Mag_BZ_GSM', 
 #             'Mag_RMS_Mag', 'Mag_RMS_Vector', 'Mag_RMS_BX_GSE', 'Mag_RMS_BY_GSE', 'Mag_RMS_BZ_GSE',
 #             'Plasma_SW_Temp', 'Plasma_SW_Density', 'Plasma_SW_Speed',
@@ -150,40 +175,17 @@ omni_df.sort_index(inplace=True)
 #             'Solar_Lyman_Alpha',
 #             'Particle_Proton_Flux_1MeV', 'Particle_Proton_Flux_2MeV', 
 #             'Particle_Proton_Flux_4MeV', 'Particle_Proton_Flux_10MeV', 
-#             'Particle_Proton_Flux_30MeV', 'Particle_Proton_Flux_60MeV', 'Particle_Flux_Flag']:
-#     if col not in filtered_df.columns:
-#         filtered_df[col] = pd.Series(np.nan, index=filtered_df.index)
-#     filtered_df[col] = filtered_df[col].fillna(omni_df[col])
+#             'Particle_Proton_Flux_30MeV', 'Particle_Proton_Flux_60MeV', 'Particle_Flux_Flag'], desc="Expanding low res columns temporally"):
+#     values = omni_df[col].reindex(pd.DatetimeIndex(low_res_timestamps.ravel())).values.reshape(low_res_timestamps.shape)
+#     for i in range(values.shape[1]):
+#         new_columns[f'{col}_{i}'] = values[:, i]
+#     missing_values = np.isnan(values).any(axis=1)
+#     if missing_values.any():
+#         print("Some values are missing:")
+#         print(f"Missing {col}: {missing_values.sum()} rows")
 
-# Temporal
-low_res_time_range = pd.timedelta_range(start='0D', end='3D', freq='1H')
-dt_index = pd.DatetimeIndex(filtered_df.index)
-low_res_timestamps = dt_index.values[:, None] - low_res_time_range.values
-
-new_columns = {}
-for col in tqdm(['Mag_Scalar_B', 'Mag_Vector_B', 'Mag_B_Lat_GSE', 'Mag_B_Long_GSE',
-            'Mag_BX_GSE', 'Mag_BY_GSE', 'Mag_BZ_GSE', 'Mag_BY_GSM', 'Mag_BZ_GSM', 
-            'Mag_RMS_Mag', 'Mag_RMS_Vector', 'Mag_RMS_BX_GSE', 'Mag_RMS_BY_GSE', 'Mag_RMS_BZ_GSE',
-            'Plasma_SW_Temp', 'Plasma_SW_Density', 'Plasma_SW_Speed',
-            'Plasma_SW_Flow_Long', 'Plasma_SW_Flow_Lat', 'Plasma_Alpha_Prot_Ratio', 
-            'Plasma_Sigma_T', 'Plasma_Sigma_N', 'Plasma_Sigma_V', 
-            'Plasma_Sigma_Phi_V', 'Plasma_Sigma_Theta_V', 'Plasma_Sigma_Ratio',
-            'Solar_Kp', 'Solar_R_Sunspot', 'Solar_Dst', 'Solar_Ap', 
-            'Solar_AE', 'Solar_AL', 'Solar_AU', 'Solar_PC',
-            'Solar_Lyman_Alpha',
-            'Particle_Proton_Flux_1MeV', 'Particle_Proton_Flux_2MeV', 
-            'Particle_Proton_Flux_4MeV', 'Particle_Proton_Flux_10MeV', 
-            'Particle_Proton_Flux_30MeV', 'Particle_Proton_Flux_60MeV', 'Particle_Flux_Flag'], desc="Expanding low res columns temporally"):
-    values = omni_df[col].reindex(pd.DatetimeIndex(low_res_timestamps.ravel())).values.reshape(low_res_timestamps.shape)
-    for i in range(values.shape[1]):
-        new_columns[f'{col}_{i}'] = values[:, i]
-    missing_values = np.isnan(values).any(axis=1)
-    if missing_values.any():
-        print("Some values are missing:")
-        print(f"Missing {col}: {missing_values.sum()} rows")
-
-# Add all new columns at once
-filtered_df = pd.concat([filtered_df, pd.DataFrame(new_columns, index=filtered_df.index)], axis=1)
+# # Add all new columns at once
+# filtered_df = pd.concat([filtered_df, pd.DataFrame(new_columns, index=filtered_df.index)], axis=1)
 
 # Reset the index if you want to keep 'DateTimeFormatted' as a column
 filtered_df.reset_index(drop=True, inplace=True)
@@ -193,67 +195,67 @@ filtered_df.set_index('DateTimeFormatted', inplace=True)
 # 1 Min data
 # -----------------------------------
 
-# More 1 min data
-# Read all .lst files from the directory
-file_list = glob.glob('../data/omni_1min/*.txt')
+# # More 1 min data
+# # Read all .lst files from the directory
+# file_list = glob.glob('../data/omni_1min/*.txt')
 
-df_list = []
+# df_list = []
 
-for file in file_list:
-    # Define column names
-    columns = ['Year', 'Day', 'Hour', 'Minute',
-               'BSN_X_GSE', 'BSN_Y_GSE', 'BSN_Z_GSE',
-               'AE_index', 'AU_index', 'SYM_D', 'ASY_D', 'ASY_H', 'PCN_index']
+# for file in file_list:
+#     # Define column names
+#     columns = ['Year', 'Day', 'Hour', 'Minute',
+#                'BSN_X_GSE', 'BSN_Y_GSE', 'BSN_Z_GSE',
+#                'AE_index', 'AU_index', 'SYM_D', 'ASY_D', 'ASY_H', 'PCN_index']
     
-    # Read the OMNI data file
-    df = pd.read_csv(file, delim_whitespace=True, names=columns)
+#     # Read the OMNI data file
+#     df = pd.read_csv(file, delim_whitespace=True, names=columns)
     
-    # Create the 'DateTime' column from Year, Day of Year, Hour, and Minute
-    df['DateTime'] = pd.to_datetime(df['Year'] * 1000 + df['Day'], format='%Y%j') \
-                     + pd.to_timedelta(df['Hour'], unit='h') \
-                     + pd.to_timedelta(df['Minute'], unit='m')
+#     # Create the 'DateTime' column from Year, Day of Year, Hour, and Minute
+#     df['DateTime'] = pd.to_datetime(df['Year'] * 1000 + df['Day'], format='%Y%j') \
+#                      + pd.to_timedelta(df['Hour'], unit='h') \
+#                      + pd.to_timedelta(df['Minute'], unit='m')
     
-    # Keep only the necessary columns
-    df = df[['DateTime', 'BSN_X_GSE', 'BSN_Y_GSE', 'BSN_Z_GSE',
-               'AE_index', 'AU_index', 'SYM_D', 'ASY_D', 'ASY_H', 'PCN_index']]
+#     # Keep only the necessary columns
+#     df = df[['DateTime', 'BSN_X_GSE', 'BSN_Y_GSE', 'BSN_Z_GSE',
+#                'AE_index', 'AU_index', 'SYM_D', 'ASY_D', 'ASY_H', 'PCN_index']]
     
-    df_list.append(df)
-# Concatenate all OMNI DataFrames into one
-omni_df = pd.concat(df_list, ignore_index=True)
+#     df_list.append(df)
+# # Concatenate all OMNI DataFrames into one
+# omni_df = pd.concat(df_list, ignore_index=True)
 
-# Reset index to make 'DateTime' a column
-omni_df.reset_index(drop=True, inplace=True)
-omni_df = omni_df.drop_duplicates(subset='DateTime', keep='first')
-omni_df.set_index('DateTime', inplace=True)
-filtered_df.sort_index(inplace=True)
-omni_df.sort_index(inplace=True)
+# # Reset index to make 'DateTime' a column
+# omni_df.reset_index(drop=True, inplace=True)
+# omni_df = omni_df.drop_duplicates(subset='DateTime', keep='first')
+# omni_df.set_index('DateTime', inplace=True)
+# filtered_df.sort_index(inplace=True)
+# omni_df.sort_index(inplace=True)
 
-# Instantaneous
-# for col in ['BSN_X_GSE', 'BSN_Y_GSE', 'BSN_Z_GSE', 'AE_index', 'AU_index', 'SYM_D',
-#        'ASY_D', 'ASY_H', 'PCN_index']:
-#     if col not in filtered_df.columns:
-#         filtered_df[col] = pd.Series(np.nan, index=filtered_df.index)
-#     filtered_df[col] = filtered_df[col].fillna(omni_df[col])
-#     print(f"{col}: {filtered_df[col].min()} to {filtered_df[col].max()}")
+# # Instantaneous
+# # for col in ['BSN_X_GSE', 'BSN_Y_GSE', 'BSN_Z_GSE', 'AE_index', 'AU_index', 'SYM_D',
+# #        'ASY_D', 'ASY_H', 'PCN_index']:
+# #     if col not in filtered_df.columns:
+# #         filtered_df[col] = pd.Series(np.nan, index=filtered_df.index)
+# #     filtered_df[col] = filtered_df[col].fillna(omni_df[col])
+# #     print(f"{col}: {filtered_df[col].min()} to {filtered_df[col].max()}")
 
 
-# Temporal
-high_res_time_range = pd.timedelta_range(start='0D', end='3D', freq='30min')
-dt_index = pd.DatetimeIndex(filtered_df.index)
-high_res_timestamps = dt_index.values[:, None] - high_res_time_range.values
+# # Temporal
+# high_res_time_range = pd.timedelta_range(start='0D', end='3D', freq='30min')
+# dt_index = pd.DatetimeIndex(filtered_df.index)
+# high_res_timestamps = dt_index.values[:, None] - high_res_time_range.values
 
-new_columns = {}
-for col in tqdm(['BSN_X_GSE', 'BSN_Y_GSE', 'BSN_Z_GSE', 'AE_index', 'AU_index', 'SYM_D', 'ASY_D', 'ASY_H', 'PCN_index'], desc="Expanding high res columns temporally"):
-    values = omni_df[col].reindex(pd.DatetimeIndex(high_res_timestamps.ravel())).values.reshape(high_res_timestamps.shape)
-    for i in range(values.shape[1]):
-        new_columns[f'{col}_{i}'] = values[:, i]
-    missing_values = np.isnan(values).any(axis=1)
-    if missing_values.any():
-        print("Some values are missing:")
-        print(f"Missing {col}: {missing_values.sum()} rows")
+# new_columns = {}
+# for col in tqdm(['BSN_X_GSE', 'BSN_Y_GSE', 'BSN_Z_GSE', 'AE_index', 'AU_index', 'SYM_D', 'ASY_D', 'ASY_H', 'PCN_index'], desc="Expanding high res columns temporally"):
+#     values = omni_df[col].reindex(pd.DatetimeIndex(high_res_timestamps.ravel())).values.reshape(high_res_timestamps.shape)
+#     for i in range(values.shape[1]):
+#         new_columns[f'{col}_{i}'] = values[:, i]
+#     missing_values = np.isnan(values).any(axis=1)
+#     if missing_values.any():
+#         print("Some values are missing:")
+#         print(f"Missing {col}: {missing_values.sum()} rows")
 
-# Add all new columns at once
-filtered_df = pd.concat([filtered_df, pd.DataFrame(new_columns, index=filtered_df.index)], axis=1)
+# # Add all new columns at once
+# filtered_df = pd.concat([filtered_df, pd.DataFrame(new_columns, index=filtered_df.index)], axis=1)
 
 # SYMH
 # Read all .lst files from the directory
@@ -389,10 +391,9 @@ for i in tqdm(range(num_chunks)):
     
     # Save the dataset as a separate shard
     dataset.save_to_disk(
-        f'../data/akebono_solar_combined_v5_experimental_train/chunk_{i:03d}',
+        f'../data/akebono_solar_combined_v6_chu_train/chunk_{i:03d}',
         num_shards=1
     )
-
 print(f"Train dataset saved in {num_chunks} separate chunks.")
 
 # Convert val and test DataFrames to Hugging Face datasets
@@ -400,7 +401,7 @@ val_dataset = datasets.Dataset.from_pandas(val)
 test_dataset = datasets.Dataset.from_pandas(test)
 
 # Save validation and test datasets to disk
-val_dataset.save_to_disk('../data/akebono_solar_combined_v5_experimental_val')
-test_dataset.save_to_disk('../data/akebono_solar_combined_v5_experimental_test')
+val_dataset.save_to_disk('../data/akebono_solar_combined_v6_chu_val')
+test_dataset.save_to_disk('../data/akebono_solar_combined_v6_chu_test')
 
 print("Validation and test datasets converted to Hugging Face datasets.")
