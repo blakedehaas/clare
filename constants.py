1d2700319fe218034cbdbba6f8d66604a23ed215
Michael Liang <michaelliang15@gmail.com>
2025-03-30 18:41:47 +0000

    1.41

--- constants.py
+++ constants.py
@@ -9,4 +9,5 @@ NORMALIZATIONS = {
     'GMLT': lambda x: np.sin(np.array(x, dtype=np.float32) * np.pi / 12),  # Convert MLT (0-24) to sine
     'XXLAT': lambda x: np.array(x, dtype=np.float32) / 90,  # Scale latitude to [-1, 1]
     'XXLON': lambda x: np.sin(np.deg2rad(np.array(x, dtype=np.float32))),  # Convert longitude to sine
+    "Kp_index": lambda x: np.array(x, dtype=np.float32) / 45 - 1,  # Scale Kp index (0-90) to [-1, 1]
 }
